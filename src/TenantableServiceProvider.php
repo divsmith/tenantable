e5d15499ed5398dd4f2ced0f3aa0c99b0427db89
Parker Smith <parker@parkersmith.us>
2015-05-26 01:06:26 -0600

    Add service provider

---
+++ src/TenantableServiceProvider.php
@@ -0,0 +1,27 @@
+<?php  namespace Tenantable; 
+
+use Illuminate\Support\ServiceProvider;
+
+class TenantableServiceProvider extends ServiceProvider {
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->bind('Tenantable\TenantResolver\TenantResolverInterface', function ($app){
+            return new TenantResolver\EloquentTenantResolver($app['config'], $app, $app['request']);
+        });
+
+        $this->app->bind('Tenantable\UserResolver\UserResolverInterface', function($app){
+            return new UserResolver\EloquentUserResolver($app['Illuminate\Contracts\Auth\Guard'], $app['config']);
+        });
+
+        $this->app->bind('Tenantable\Tenantable', function($app)
+        {
+            return new Tenantable($app['Tenantable\TenantResolver\TenantResolverInterface'], $app['Tenantable\UserResolver\UserResolverInterface']);
+        });
+    }
+}
