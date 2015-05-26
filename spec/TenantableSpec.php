380ddb2bd23ca88cc48135660dd9492010d437d5
Parker Smith <parker@parkersmith.us>
2015-05-25 21:31:34 -0600

    Add specs and logic

--- spec/TenantableSpec.php
+++ spec/TenantableSpec.php
@@ -4,11 +4,56 @@ namespace spec\Tenantable;
 
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
+use Tenantable\TenantResolver\TenantResolverInterface;
+use Tenantable\UserResolver\UserResolverInterface;
 
 class TenantableSpec extends ObjectBehavior
 {
+    function let(TenantResolverInterface $tenantResolver, UserResolverInterface $userResolver)
+    {
+        $this->beConstructedWith($tenantResolver, $userResolver);
+    }
+
     function it_is_initializable()
     {
         $this->shouldHaveType('Tenantable\Tenantable');
+    }
+
+    function it_returns_false_when_user_not_tenant_member(TenantResolverInterface $tenantResolver, UserResolverInterface $userResolver)
+    {
+        $this->beConstructedWith($tenantResolver, $userResolver);
+
+        $userResolver->getUserId()->willReturn(5);
+        $tenantResolver->getTenantUserIds()->willReturn([1, 2, 3, 4]);
+
+        $this->tenantHasUser()->shouldReturn(false);
+    }
+
+    function it_returns_true_when_user_is_tenant_member(TenantResolverInterface $tenantResolver, UserResolverInterface $userResolver)
+    {
+        $this->beConstructedWith($tenantResolver, $userResolver);
+
+        $userResolver->getUserId()->willReturn(1);
+        $tenantResolver->getTenantUserIds()->willReturn([1, 2, 3, 4]);
+
+        $this->tenantHasUser()->shouldReturn(true);
+    }
+
+    function it_returns_false_when_user_not_defined(TenantResolverInterface $tenantResolver, UserResolverInterface $userResolver)
+    {
+        $this->beConstructedWith($tenantResolver, $userResolver);
+
+        $userResolver->getUserId()->willReturn(null);
+        $tenantResolver->getTenantUserIds()->willReturn([1, 2, 3, 4]);
+    }
+
+    function it_returns_the_tenant_id(TenantResolverInterface $tenantResolver, UserResolverInterface $userResolver)
+    {
+        $this->beConstructedWith($tenantResolver, $userResolver);
+
+        $tenantResolver->getTenantId()->shouldBeCalled();
+        $tenantResolver->getTenantId()->willReturn(2);
+
+        $this->getTenantId()->shouldReturn(2);
     }
 }
--- src/Tenantable.php
+++ src/Tenantable.php
@@ -1,5 +1,27 @@
 <?php namespace Tenantable;
 
+use Tenantable\TenantResolver\TenantResolverInterface;
+use Tenantable\UserResolver\UserResolverInterface;
+
 class Tenantable
 {
+    protected $tenantResolver;
+
+    protected $userResolver;
+
+    public function tenantHasUser()
+    {
+        return in_array($this->userResolver->getUserId(), $this->tenantResolver->getTenantUserIds());
+    }
+
+    public function __construct(TenantResolverInterface $tenantResolver, UserResolverInterface $userResolver)
+    {
+        $this->tenantResolver = $tenantResolver;
+        $this->userResolver = $userResolver;
+    }
+
+    public function getTenantId()
+    {
+        return $this->tenantResolver->getTenantId();
+    }
 }
