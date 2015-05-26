727620e7c2a9584730893d67301170c0c26a8fd4
Parker Smith <parker@parkersmith.us>
2015-05-25 21:33:42 -0600

    Add docblocsk

--- src/Tenantable.php
+++ src/Tenantable.php
@@ -5,21 +5,42 @@ use Tenantable\UserResolver\UserResolverInterface;
 
 class Tenantable
 {
+    /**
+     * @var TenantResolverInterface
+     */
     protected $tenantResolver;
 
+    /**
+     * @var UserResolverInterface
+     */
     protected $userResolver;
 
-    public function tenantHasUser()
-    {
-        return in_array($this->userResolver->getUserId(), $this->tenantResolver->getTenantUserIds());
-    }
-
+    /**
+     * @param TenantResolverInterface $tenantResolver
+     * @param UserResolverInterface $userResolver
+     */
     public function __construct(TenantResolverInterface $tenantResolver, UserResolverInterface $userResolver)
     {
         $this->tenantResolver = $tenantResolver;
         $this->userResolver = $userResolver;
     }
 
+    /**
+     * Determine whether the current user is associated with the tenant the
+     * request was directed to.
+     *
+     * @return bool
+     */
+    public function tenantHasUser()
+    {
+        return in_array($this->userResolver->getUserId(), $this->tenantResolver->getTenantUserIds());
+    }
+
+    /**
+     * Get the ID for the tenant associated with the request.
+     *
+     * @return int
+     */
     public function getTenantId()
     {
         return $this->tenantResolver->getTenantId();
