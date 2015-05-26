62f2a566e295024fc324004523d4817b442ece9c
Parker Smith <parker@parkersmith.us>
2015-05-26 01:33:36 -0600

    Add comments

--- src/Config/laravel.php
+++ src/Config/laravel.php
@@ -3,20 +3,45 @@
 return [
 
     'tenant' => [
+        /**
+         * The Eloquent model associated with individual tenants.
+         */
         'model' => 'App\Tenant',
+
+        /**
+         * Plural lowercase name of the model used for associations.
+         */
         'plural' => 'tenants',
 
         'route' => [
+
+            /**
+             * Route parameter used to identify tenants, i.e.
+             *
+             *  Route::group([domain => {tenant}.myapp.com ...) or
+             *  Route::group([domain => myapp.com/{tenant} ...)
+             */
             'parameter' => 'tenant'
         ],
 
         'database' => [
+
+            /**
+             * Database column associated with the route parameter.
+             */
             'column' => 'slug'
         ]
     ],
 
     'user' => [
+        /**
+         * The Eloquent model associated with users.
+         */
         'model' => 'App\User',
+
+        /**
+         * Plural lowercase name of the model used for associations.
+         */
         'plural' => 'users'
     ]
 ];
