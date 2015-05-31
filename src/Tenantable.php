3397d2909c8c54d8c8481da5f7bfd743caca7e1d
Parker Smith <parker@parkersmith.us>
2015-05-31 00:01:11 -0600

    Add interface, renaming

--- src/Tenantable.php
+++ src/Tenantable.php
@@ -1,48 +1,7 @@
-<?php namespace Tenantable;
+<?php  namespace Tenantable; 
 
-use Tenantable\TenantResolver\TenantResolverInterface;
-use Tenantable\UserResolver\UserResolverInterface;
+interface Tenantable {
 
-class Tenantable
-{
-    /**
-     * @var TenantResolverInterface
-     */
-    protected $tenantResolver;
+    public function userBelongsToTenant();
 
-    /**
-     * @var UserResolverInterface
-     */
-    protected $userResolver;
-
-    /**
-     * @param TenantResolverInterface $tenantResolver
-     * @param UserResolverInterface $userResolver
-     */
-    public function __construct(TenantResolverInterface $tenantResolver, UserResolverInterface $userResolver)
-    {
-        $this->tenantResolver = $tenantResolver;
-        $this->userResolver = $userResolver;
-    }
-
-    /**
-     * Determine whether the current user is associated with the tenant the
-     * request was directed to.
-     *
-     * @return bool
-     */
-    public function tenantHasUser()
-    {
-        return in_array($this->userResolver->getUserId(), $this->tenantResolver->getTenantUserIds());
-    }
-
-    /**
-     * Get the ID for the tenant associated with the request.
-     *
-     * @return int
-     */
-    public function getTenantId()
-    {
-        return $this->tenantResolver->getTenantId();
-    }
 }
---
+++ src/UserHasTenantTenantable.php
@@ -0,0 +1,48 @@
+<?php namespace Tenantable;
+
+use Tenantable\TenantResolver\TenantResolverInterface;
+use Tenantable\UserResolver\UserResolverInterface;
+
+class UserHasTenantTenantable implements Tenantable
+{
+    /**
+     * @var TenantResolverInterface
+     */
+    protected $tenantResolver;
+
+    /**
+     * @var UserResolverInterface
+     */
+    protected $userResolver;
+
+    /**
+     * @param TenantResolverInterface $tenantResolver
+     * @param UserResolverInterface $userResolver
+     */
+    public function __construct(TenantResolverInterface $tenantResolver, UserResolverInterface $userResolver)
+    {
+        $this->tenantResolver = $tenantResolver;
+        $this->userResolver = $userResolver;
+    }
+
+    /**
+     * Determine whether the current user is associated with the tenant the
+     * request was directed to.
+     *
+     * @return bool
+     */
+    public function userBelongsToTenant()
+    {
+        return in_array($this->userResolver->getUserId(), $this->tenantResolver->getTenantUserIds());
+    }
+
+    /**
+     * Get the ID for the tenant associated with the request.
+     *
+     * @return int
+     */
+    public function getTenantId()
+    {
+        return $this->tenantResolver->getTenantId();
+    }
+}
